fbc6a82e6dafa5c87633814a5f167df83394656f
SeaBee <85791528+seabee33@users.noreply.github.com>
2024-10-10 23:08:29 +1100

    Update setup_wireguard.sh

--- wireguard/setup_wireguard.sh
+++ wireguard/setup_wireguard.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
+# Checks to see if script is being run as root
 if [ "$EUID" -ne 0 ]; then
   echo "Please run as root"
   exit
 fi
 
+# Deletes keys in case this script is being run again
 rm -f /etc/wireguard/server_priv.key /etc/wireguard/server_pub.key /etc/wireguard/wg0.conf /etc/wireguard/client/priv.key /etc/wireguard/client/pub.key /etc/wireguard/client/wg0.conf 
+
+# Create necessary directories
 mkdir -p /etc/wireguard/client
 
+
+# Create and store server private and public key
 wg genkey | tee /etc/wireguard/server_priv.key | wg pubkey | tee /etc/wireguard/server_pub.key
 
+# Get the server public and private key as well as the network interface
 server_priv_key=$(cat /etc/wireguard/server_priv.key)
 server_pub_key=$(cat /etc/wireguard/server_pub.key)
 network_interface=$(ip -o -4 route show to default | awk '{print $5}')
 
+# Create the config file for wireguard
 echo "[Interface]
 Address = 10.0.0.1/24
 SaveConfig = true
@@ -36,10 +44,12 @@ systemctl enable wg-quick@wg0
 
 
 # Client
+# Create client public and private key and store it in vars for later
 wg genkey | tee /etc/wireguard/client/priv.key | wg pubkey | tee /etc/wireguard/client/pub.key
 client_priv_key=$(cat /etc/wireguard/client/priv.key)
 client_pub_key=$(cat /etc/wireguard/client/pub.key)
 
+# Create initial client config
 echo "[Interface]
 PrivateKey = $client_priv_key
 Address = 10.0.0.2/24
@@ -48,8 +58,7 @@ Address = 10.0.0.2/24
 PublicKey = $server_pub_key
 Endpoint = servername_or_ip:51820
 AllowedIPs = 0.0.0.0/0
-
 " > /etc/wireguard/client/wg0.conf
 
-
+# Add client public key to servers trusted clients
 wg set wg0 peer $client_pub_key allowed-ips 10.0.0.2/32
