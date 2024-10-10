ad186d9969579b9675379354399cc6799c73e560
SeaBee <85791528+seabee33@users.noreply.github.com>
2024-10-10 14:34:10 +1100

    Create wirwireguard_setup.sh

---
+++ wirwireguard_setup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit
+fi
+
+rm -f /etc/wireguard/server_priv.key /etc/wireguard/server_pub.key /etc/wireguard/wg0.conf /etc/wireguard/client/priv.key /etc/wireguard/client/pub.key /etc/wireguard/client/wg0.conf 
+mkdir -p /etc/wireguard/client
+
+wg genkey | tee /etc/wireguard/server_priv.key | wg pubkey | tee /etc/wireguard/server_pub.key
+
+server_priv_key=$(cat /etc/wireguard/server_priv.key)
+server_pub_key=$(cat /etc/wireguard/server_pub.key)
+network_interface=$(ip -o -4 route show to default | awk '{print $5}')
+
+echo "[Interface]
+Address = 10.0.0.1/24
+SaveConfig = true
+ListenPort = 51820
+PrivateKey = $server_priv_key
+PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o $network_interface -j MASQUERADE
+PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o $network_interface -j MASQUERADE
+" > /etc/wireguard/wg0.conf
+
+# Change permissions so only root can access the files
+chmod 600 /etc/wireguard/server_priv.key
+chmod 600 /etc/wireguard/wg0.conf
+
+# Allow 51820 through ufw
+ufw allow 51820/udp
+
+# Start wireguard and set it to auto start on boot
+wg-quick up wg0
+systemctl enable wg-quick@wg0
+
+
+# Client
+wg genkey | tee /etc/wireguard/client/priv.key | wg pubkey | tee /etc/wireguard/client/pub.key
+client_priv_key=$(cat /etc/wireguard/client/priv.key)
+client_pub_key=$(cat /etc/wireguard/client/pub.key)
+
+echo "[Interface]
+PrivateKey = $client_priv_key
+Address = 10.0.0.2/24
+
+[Peer]
+PublicKey = $server_pub_key
+Endpoint = home.conorbriggs.com.au:51820
+AllowedIPs = 0.0.0.0/0
+
+" > /etc/wireguard/client/wg0.conf
+
+
+wg set wg0 peer $client_pub_key allowed-ips 10.0.0.2
